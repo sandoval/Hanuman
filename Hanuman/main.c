0b3546eac02ab6cf5072728a53b09b017398233e
Pedro Salum <pedro@loopec.com.br>
2013-05-05 22:49:16 -0300

    Added the options menu for the Hanuman
    Added menu options. It will be used only on production mode. For test
    mode, define TESTMODE for fast results.

--- Hanuman/main.c
+++ Hanuman/main.c
@@ -2,21 +2,57 @@
 //  main.c
 //  Hanuman
 //
-//  Created by Daniel Sandoval on 04/05/2013.
+//  Created by Daniel Sandoval and Pedro Salum on 04/05/2013.
 //  Copyright (c) 2013 LoopEC. All rights reserved.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define SOURCE_FILE_MAX_CHARS 100000
+#define TESTMODE
 
 char* loadFileToMemory(const char* path);
 
 int main(int argc, const char * argv[])
 {
-    char* code = loadFileToMemory("/Users/sandoval/Desktop/Hanuman/Hanuman/teste.txt");
-    puts(code);
+    
+#ifndef TESTMODE
+    if (argc < 2) {
+        printf("\n\tEntre com a opção --help para obter ajuda\n\n");
+        exit(1);
+    }
+    else{
+        if((strcmp(argv[1],"-h") == 0) || ( strcmp(argv[1],"-H") == 0) || ( strcmp(argv[1], "--help") ==0 )){
+            printf("\nManual de funcionamento do Hanuman:\n\n");
+            printf("\t --help, -h, -H\t - Instruções\n\n");
+            printf("\t [nome do .s] [nome do .bin]\n");
+            printf("\t\t Monta o arquivo .s especificado e salva o mesmo no .bin\n\n");
+            printf("\t\t Caso nao seja especificado um segundo paramentro, o arquivo \n\t\tde saida tera o mesmo nome do de entrada\n\n");
+            printf("\n\n -- Created by Daniel Sandoval and Pedro Salum --\n -- Copyright (c) 2013 LoopEC. All rights reserved. -- \n\n");
+            exit(2);
+            
+        }
+        else if (strlen(argv[1]) >= 3) {
+
+            char* code = loadFileToMemory(argv[1]);
+            puts(code);
+            
+        }
+        
+    }
+#endif
+
+#ifdef TESTMODE
+        char* code = loadFileToMemory("/Users/sandoval/Desktop/Hanuman/Hanuman/teste.txt");
+        puts(code);
+
+#endif
+
+    
+    
     return 0;
+
 }
 
 char* loadFileToMemory(const char* path) {
